84d4e280744f2cabd5481e5f84903c280b1a3106
Jeremy Walker <jez.walker@gmail.com>
2020-05-28 22:05:56 +0100

    Fix linting errors

--- src/components/TrackSelection.tsx
+++ src/components/TrackSelection.tsx
@@ -50,26 +50,44 @@ export function TrackSelection(): JSX.Element {
         <h1 className="mb-4">Exercism: V3 Dashboard</h1>
       </header>
       <p>
-        👋 <strong>Welcome!</strong> Thanks for being involved in the creation
-        of version 3 (v3) of Exercism! 🥳
+        <span role="img" aria-label="hand waving">
+          👋
+        </span>{' '}
+        <strong>Welcome!</strong> Thanks for being involved in the creation of
+        version 3 (v3) of Exercism!{' '}
+        <span role="img" aria-label="smiley face with party hat, celebrating">
+          🥳
+        </span>
       </p>
       <p>
         <strong>This dashboard</strong> is the central hub for how we're
         managing v3 of Exercism. Maintainers can use it to understand how their
         tracks are progressing, and contributors can use it to find out where
         help is required. If you're <strong>new to v3</strong>{' '}
-        <a href="https://www.youtube.com/watch?v=5Cj1Dr9m3GM" target="_blank">
+        <a
+          href="https://www.youtube.com/watch?v=5Cj1Dr9m3GM"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           this video
         </a>{' '}
         and{' '}
-        <a href="https://github.com/exercism/v3/issues/690" target="_blank">
+        <a
+          href="https://github.com/exercism/v3/issues/690"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           this issue
         </a>{' '}
         give some introductory information. Contributions to the dashboard are
         welcome{' '}
         <a href="https://github.com/exercism/v3-dashboard">via GitHub</a>.{' '}
         <strong>If you get stuck</strong>, you can{' '}
-        <a href="https://join.slack.com/t/exercism-team/shared_invite/zt-59aazo7p-VtFgs_vwZwdTwTBLkGYspQ">
+        <a
+          href="https://join.slack.com/t/exercism-team/shared_invite/zt-59aazo7p-VtFgs_vwZwdTwTBLkGYspQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           join our Slack Workspace
         </a>{' '}
         and ask for help in the #v3 channel.
