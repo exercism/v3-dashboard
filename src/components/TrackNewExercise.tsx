b46fc90c6d29041d07e7827713a220427972291b
Erik Schierboom <erik_schierboom@hotmail.com>
2020-05-28 17:41:39 +0200

    Initial work on form

--- src/components/TrackNewExercise.tsx
+++ src/components/TrackNewExercise.tsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useState, FormEvent } from 'react'
 
 export function TrackNewExercise({
   trackId,
 }: {
   trackId: TrackIdentifier
 }): JSX.Element {
-  return <p>TODO: add new exercise wizard</p>
+  const [exampleFilename, setExampleFilename] = useState('')
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault()
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label>Exercise name</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="E.g. interfaces"
+          value={exampleFilename}
+          onChange={(e) => setExampleFilename(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Design</label>
+        <textarea
+          className="form-control"
+          value={exampleFilename}
+          onChange={(e) => setExampleFilename(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Instructions</label>
+        <textarea
+          className="form-control"
+          value={exampleFilename}
+          onChange={(e) => setExampleFilename(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Example</label>
+        <textarea
+          className="form-control"
+          value={exampleFilename}
+          onChange={(e) => setExampleFilename(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Example filename</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="E.g. Example.cs"
+          value={exampleFilename}
+          onChange={(e) => setExampleFilename(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <input type="submit" value="Submit" />
+      </div>
+    </form>
+  )
 }
