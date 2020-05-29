0571c2d0150f5aff66a3358277da3aae8bbe9143
Erik Schierboom <erik_schierboom@hotmail.com>
2020-05-29 11:35:20 +0200

    Add scaffolding for contributing

--- src/components/TrackContributing.tsx
+++ src/components/TrackContributing.tsx
@@ -7,5 +7,11 @@ export interface TrackContributingProps {
 export function TrackContributing({
   trackId,
 }: TrackContributingProps): JSX.Element {
-  return <p>TODO: add contributing information</p>
+  return (
+    <div className="d-flex flex-wrap align-items-center mt-4 mb-4 row">
+      <div className="col-12 mb-2">
+        <p>TODO: add contributing information</p>
+      </div>
+    </div>
+  )
 }
