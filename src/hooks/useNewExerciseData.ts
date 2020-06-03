4e3a941bd045977d821c50e514f467f3105989f4
Erik Schierboom <erik_schierboom@hotmail.com>
2020-06-03 18:30:01 +0200

    Bugfix

--- src/hooks/useNewExerciseData.ts
+++ src/hooks/useNewExerciseData.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import { useCallback } from 'react'
 import { useMutableMemoryValue, StoredMemoryValue } from 'use-memory-value'
 
 interface NewExerciseData {
