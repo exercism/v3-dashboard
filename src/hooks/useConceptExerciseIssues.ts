b561c5ce3adc75b45c2dedc38928df8b16a325e8
Erik Schierboom <erik_schierboom@hotmail.com>
2020-06-12 14:00:50 +0200

    Update to latest test method names

--- src/hooks/useConceptExerciseIssues.ts
+++ src/hooks/useConceptExerciseIssues.ts
@@ -59,7 +59,7 @@ export function useCreationConceptExerciseIssues(
   trackId: TrackIdentifier
 ): ConceptExerciseIssueResult<ConceptExerciseIssue[]> {
   return useConceptExerciseIssuesApi(
-    `http://lvh.me:3000/git_api/tracks/${trackId}/creation_issues`
+    `http://lvh.me:3000/git_api/tracks/${trackId}/open_creation_issues`
   )
 }
 
@@ -67,7 +67,7 @@ export function useCreationConceptExerciseIssuesCount(
   trackId: TrackIdentifier
 ): ConceptExerciseIssueResult<number> {
   return useConceptExerciseIssuesApi(
-    `http://lvh.me:3000/git_api/tracks/${trackId}/creation_issues_count`
+    `http://lvh.me:3000/git_api/tracks/${trackId}/num_creation_issues`
   )
 }
 
@@ -75,6 +75,6 @@ export function useImproveConceptExerciseIssues(
   trackId: TrackIdentifier
 ): ConceptExerciseIssueResult<ConceptExerciseIssue[]> {
   return useConceptExerciseIssuesApi(
-    `http://lvh.me:3000/git_api/tracks/${trackId}/improve_issues`
+    `http://lvh.me:3000/git_api/tracks/${trackId}/open_improve_issues`
   )
 }
