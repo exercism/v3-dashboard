dbd4acbcda7b1b2f05c2c410f385bec3cb6cea51
Tim Austin <tim@neenjaw.com>
2020-06-21 00:13:09 -0600

    remove unneeded module

--- src/components/graph/graph-util.ts
+++
@@ -1,4 +0,0 @@
-// use sine-based function to provide an offset based on an index within a range specified by amount
-export const adjustment = (i: number, amount: number): number => {
-  return Math.sin((i % 5) / -3) * amount
-}
