568caded9ae90e193c9eefa53aee2df115a38503
Angelika Tyborska <angelikatyborska@fastmail.com>
2020-07-25 19:03:01 +0200

    Use flex to position header navigation elements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 export function Header(): JSX.Element {
   return (
     <header className="p-3 mb-3 align-right" style={{ background: '#009cab' }}>
-      <div className="container">
+      <div className="container d-flex justify-content-between">
         <HeaderLink to="/">
           <HeaderLogo />
         </HeaderLink>
 
-        <nav className="float-right">
+        <nav>
           <HeaderLink to="/">Tracks</HeaderLink>
           <HeaderLink to="/exercises">Exercises</HeaderLink>
           <HeaderLink to="/stories">Stories</HeaderLink>
@@ -24,7 +24,6 @@ function HeaderLogo(): JSX.Element {
     <img
       alt="Exercism logo"
       src="https://assets.exercism.io/assets/logo-white-e3be059a4bfc4bf65f196a12105e9cff389b5a67f2065a0862d4ff6153571ef5.png"
-      className="float-left"
       height="24px"
     />
   )
